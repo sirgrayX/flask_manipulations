2e0c7693784ab159fc5df34100f7197b49926b08
Сергей Александрович Булганин <s.bulganin@proton-group.ru>
2024-06-22 13:29:16 +0300

    Добавил шаблоны

--- main.py
+++ main.py
@@ -1,35 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # инициализация приложения Flask
 app = Flask(__name__)
+app.app_context().push()
 
 @app.route("/")
-@app.route("/index")
 def main_page(environ, start_response):
-    status = '200 OK'
-    response_headers = [
-        ('Content-type', 'text/html; charset=utf-8'),
-    ]
-    start_response(status, response_headers)
-    return ["""
-<!doctype html>
-<html lang="ru">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <title>Узнать IP адрес</title>
-  </head>
-  <body class="bg-info">
-    <div class="container">
-      <h1 class="text-white text-center mx-auto p-2 mt-4 mb-3">Ваш IP-адрес</h1>
-      <div class="bg-light card-block mx-auto text-center">
-        <h1 class="display-3 p-2">0.0.0.0</h1>
-      </div>
-    </div>
-  </body>
-</html>
-""".encode('utf-8')]
+    return [render_template('main.html').encode('utf-8')]
+
+# здесь будет памятка
+@app.route("/memo")
+def get_memo():
+    return [bytes(render_template('memo.html'), 'utf8')]
+
+# здесь должна быть обработка опросника
+@app.route("/questionnaire")
+def start_questionnaire():
+    return [bytes(render_template('questionnaire.html'), 'utf8')]
 
 # обработка ошибок
 @app.errorhandler(404)
